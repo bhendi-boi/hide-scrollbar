12109583560b7e09bc422b99d542774b1fc166e0
bhendi-boi <bhendiisgreen@gmail.com>
2023-05-23 11:45:03 +0530

    added bg to ImageCard

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -2,7 +2,7 @@ import { Image } from "../types";
 
 const ImageCard = ({ src, alt }: Image) => {
   return (
-    <li className="overflow-hidden shrink-0 rounded-xl">
+    <li className="overflow-hidden bg-gradient-to-br from-gray-50/30 to-gray-300 via-gray-100/50 shrink-0 rounded-xl">
       <img
         src={src}
         alt={alt}
